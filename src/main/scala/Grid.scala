c7ba5977dcd23684d617a938c995c233f9e05ffb
sebastian clinton <sebastianclinton@icloud.com>
2018-10-21 17:22:40 +0100

    Moved the code for drawing a grid out to its own object

---
+++ src/main/scala/Grid.scala
@@ -0,0 +1,35 @@
+import org.scalajs.dom.raw.CanvasRenderingContext2D
+
+object Grid {
+  def draw(ctx: CanvasRenderingContext2D): Unit = {
+    val minor = 10
+    val major = minor * 5
+    val stroke = "#00FF00"
+    val fill = "#009900"
+    ctx.save()
+    ctx.strokeStyle = stroke
+    ctx.fillStyle = fill
+    val width = ctx.canvas.width
+    val height = ctx.canvas.height
+
+    0.to(width, minor).foreach { x =>
+      ctx.beginPath
+      ctx.moveTo(x, 0)
+      ctx.lineTo(x, height)
+      ctx.lineWidth = if (x % major == 0) 0.5
+      else 0.25
+      ctx.stroke
+      if (x % major == 0) ctx.fillText(x.toString, x, 10)
+    }
+
+    0.to(height, minor).foreach { y =>
+      ctx.beginPath
+      ctx.moveTo(0, y)
+      ctx.lineTo(width, y)
+      ctx.lineWidth = if (y % major == 0) 0.5
+      else 0.25
+      ctx.stroke
+      if (y % major == 0) ctx.fillText(y.toString, 0, y + 10)
+    }
+  }
+}
--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -17,38 +17,7 @@ object MainApp extends JSApp {
   }
 
   def draw(ctx: CanvasRenderingContext2D, gameState: GameState): Unit = {
-    if (gameState.drawGuides) drawGrid(ctx)
+    if (gameState.drawGuides) Grid.draw(ctx)
   }
 
-  def drawGrid(ctx: CanvasRenderingContext2D): Unit = {
-    val minor = 10
-    val major = minor * 5
-    val stroke = "#00FF00"
-    val fill = "#009900"
-    ctx.save()
-    ctx.strokeStyle = stroke
-    ctx.fillStyle = fill
-    val width = ctx.canvas.width
-    val height = ctx.canvas.height
-
-    0.to(width, minor).foreach { x =>
-      ctx.beginPath
-      ctx.moveTo(x, 0)
-      ctx.lineTo(x, height)
-      ctx.lineWidth = if (x % major == 0) 0.5
-      else 0.25
-      ctx.stroke
-      if (x % major == 0) ctx.fillText(x.toString, x, 10)
-    }
-
-    0.to(height, minor).foreach { y =>
-      ctx.beginPath
-      ctx.moveTo(0, y)
-      ctx.lineTo(width, y)
-      ctx.lineWidth = if (y % major == 0) 0.5
-      else 0.25
-      ctx.stroke
-      if (y % major == 0) ctx.fillText(y.toString, 0, y + 10)
-    }
-  }
 }
