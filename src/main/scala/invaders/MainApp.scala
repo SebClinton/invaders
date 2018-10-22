c874f189a77551ccac6e40826d96aba4695844c6
Doug Clinton <doug@wellfactored.com>
2018-10-22 14:03:31 +0100

    I've wired in the key handling function. It doesn't do anything yet, but the hooks are there to deal with moving the base left and right

--- src/main/scala/invaders/MainApp.scala
+++ src/main/scala/invaders/MainApp.scala
@@ -1,10 +1,9 @@
 package invaders
 
-import org.scalajs.dom.document
-import org.scalajs.dom.raw.{CanvasRenderingContext2D, HTMLCanvasElement}
+import org.scalajs.dom.raw.{CanvasRenderingContext2D, HTMLCanvasElement, KeyboardEvent}
+import org.scalajs.dom.{document, window}
 
 import scala.scalajs.js.JSApp
-
 
 case class GameState(base: Base, drawGuides: Boolean)
 
@@ -20,9 +19,34 @@ object MainApp extends JSApp {
 
     document.body.appendChild(canvas)
 
+    var gameState = GameState(Base(BlockX(BlockParty.screenWidth.v / 2 - Base.sprite.blockWidth.v / 2)), drawGuides = true)
+
     val ctx: CanvasRenderingContext2D = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+    window.onkeydown = keyDown
+    window.onkeyup = keyUp
+
+    def keyDown(e: KeyboardEvent): Unit =
+      gameState = keyHandler(gameState, e, value = true)
+
+    def keyUp(e: KeyboardEvent): Unit =
+      gameState = keyHandler(gameState, e, value = false)
+
     canvas.focus()
-    draw(ctx, GameState(Base(BlockX(BlockParty.screenWidth.v / 2 - Base.sprite.blockWidth.v / 2)), true))
+    draw(ctx, gameState)
+  }
+
+  def keyHandler(gs: GameState, e: KeyboardEvent, value: Boolean): GameState = {
+    e.key match {
+      case "ArrowLeft" | "a" =>
+        e.preventDefault()
+        gs // TODO: update the game state to move the base left
+
+      case "ArrowRight" | "d" =>
+        e.preventDefault()
+        gs // TODO: update the game state to move the base right
+
+      case _ => gs
+    }
   }
 
   def draw(ctx: CanvasRenderingContext2D, gameState: GameState): Unit = {
